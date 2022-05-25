023ce51c7b2e2236edd5795cd1377c64fb77cf46
gids-dadi <gideons564@gmail.com>
2022-05-25 19:36:07 +0100

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-/*  eslint linebreak-style: ["error", "windows"] */
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
   render() {
@@ -13,19 +12,16 @@ class Calculator extends Component {
           <div className="btn"> +/-</div>
           <div className="btn"> %</div>
           <div className="btn orange">/</div>
-
           {/* Row 2 */}
           <div className="btn">7 </div>
           <div className="btn">8 </div>
           <div className="btn">9 </div>
           <div className="btn orange">x</div>
-
           {/* Row 3 */}
           <div className="btn">4</div>
           <div className="btn">5 </div>
           <div className="btn">6 </div>
           <div className="btn orange">-</div>
-
           {/* Row 4 */}
           <div className="btn zero">0 </div>
           <div className="btn">.</div>
